3ac5c878ed8d35ea86ada0c50764261a0b3182dd
unknown <fuaad29@gmail.com>
2024-06-24 12:28:14 +0500

    added users reudx

---
+++ src/Redux/UsersSlice.tsx
@@ -0,0 +1,25 @@
+// src/usersSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: [],
+  reducers: {
+    addUser: (state, action) => {
+      state.push(action.payload);
+    },
+    removeUser: (state, action) => {
+      return state.filter(user => user.id !== action.payload);
+    },
+    updateUser: (state, action) => {
+      const index = state.findIndex(user => user.id === action.payload.id);
+      if (index !== -1) {
+        state[index] = action.payload;
+      }
+    },
+  },
+});
+
+export const { addUser, removeUser, updateUser } = usersSlice.actions;
+
+export default usersSlice.reducer;
