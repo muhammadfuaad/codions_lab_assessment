16c33cac4cb9347032322a8585fd5b8a212779bd
unknown <fuaad29@gmail.com>
2024-06-25 11:33:43 +0500

    tasks are showing in projet show

--- src/Components/Projects/Show.tsx
+++ src/Components/Projects/Show.tsx
@@ -6,9 +6,9 @@ import { useEffect, useState } from 'react';
 
 const Show: React.FC = () => {
   const [projectData, setProjectData] = useState({})
+  const [tasks, setTasks] = useState([])
   const [loading, setLoading] = useState<boolean>(true)
   const [isEdit, setIsEdit] = useState<boolean>(false)
-  // const [updatedDescription, setUpdatedDescription] = useState("")
   const users = localStorage.getItem("users")
   console.log("users:", users);
   console.log("localStorage:", localStorage);
@@ -43,6 +43,18 @@ const navigate = useNavigate()
       setLoading(false);
       console.log("error:", error);
     });
+
+    axios.get(`https://task-manager.codionslab.com/api/v1/project/${id}/task`, options)
+    .then(response => {
+      setTasks(response.data.data)
+      setLoading(false)
+    })
+    .catch(error => {
+      setLoading(false);
+      console.log("error:", error);
+    });
+    console.log("tasks:", tasks);
+
   }, [])
 
   const deleteProject = (id: number) => {
@@ -84,28 +96,48 @@ const navigate = useNavigate()
   return (
     <>
       <Card title={`${id}) ${name}`} extra={<a onClick={()=> showProject(id)}>More</a>} style={{ width: 300,
-              height: "fit-content" }}>
-              <p>{description}</p>
-              <p>Contributors:
-                <Space size={16} wrap>
-                  {contributors.map((contributor)=>{
-                    return ( 
-                      <>
-                        <Avatar size={30} gap={2}>{contributor.name}</Avatar>
-                      </>
-                    )
-                  })} 
-                </Space>
-                
-                
-              </p>
+        height: "fit-content" }}>
+        <p>{description}</p>
+        <p>Contributors:
+          <Space size={16} wrap>
+            {contributors.map((contributor)=>{
+              return ( 
+                <>
+                  <Avatar size={30} gap={2}>{contributor.name}</Avatar>
+                </>
+              )
+            })} 
+          </Space>
+        </p>
+        <h3>Tasks:</h3>
+        {tasks.map((task)=>{
+          const {id, name, description, due_date, assignee_id, status, created_at, updated_at } = task
+          return (
+            <div style={{background: "skyblue", border: "1px solid navy-blue"}}>
+              <div><h4 style={{display: "inline"}}>Task Id: </h4>{id}</div>
+              <div><h4 style={{display: "inline"}}>Description: </h4>{description}</div>
+              <div><h4 style={{display: "inline"}}>Added By: </h4>{name}</div>
+              <div><h4 style={{display: "inline"}}>Contributors: </h4>{assignee_id}</div>
+              <div><h4 style={{display: "inline"}}>Status: </h4>{status}</div>
+              <div><h4 style={{display: "inline"}}>Added at: </h4>{created_at}</div>
+              <div><h4 style={{display: "inline"}}>Updated at: </h4>{updated_at}</div>
+              <div><h4 style={{display: "inline"}}>Due date: </h4>{due_date}</div>
               <Button type="primary" onClick={() => navigate("/projects/new", {state: projectData})}>
                 Update
               </Button>
               <Button type="primary" onClick={() => deleteProject(id)}>
                 Delete
               </Button>
-            </Card>
+            </div>
+          )
+        })}
+        <Button type="primary" onClick={() => navigate("/projects/new", {state: projectData})}>
+          Update
+        </Button>
+        <Button type="primary" onClick={() => deleteProject(id)}>
+          Delete
+        </Button>
+      </Card>
     </>
   );
 };
